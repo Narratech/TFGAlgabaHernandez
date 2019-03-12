7eb934b638d28a5a9923c6115ac636a7f7d896a6
algaba97 <jalgaba@ucm.es>
2019-03-12 20:41:00 +0100

    Primer minijuego, assets sala metidos

---
+++ Assets/Scripts/CharacterGaze.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+public class CharacterGaze : MonoBehaviour
+{
+    [SerializeField] private VRInteractiveItem m_InteractiveItem;
+    DataManager DT;
+    public int player = 0;
+    bool state = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DT = DataManager.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        m_InteractiveItem.OnOver += HandleOver;
+        m_InteractiveItem.OnOut += HandleOut;
+       
+    }
+    //Handle the Over event
+    private void HandleOver()
+    {
+        Debug.Log("Show over state");
+        state = true;
+      
+    }
+
+
+    //Handle the Out event
+    private void HandleOut()
+    {
+        state = false;
+        Debug.Log("Show out state");
+      
+    }
+    void FixedUpdate()
+    {
+        if (state) DT.frameLooking(player);
+        else DT.frameNotLooking(player);
+    }
+}
---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance = null;
+    float timer = 10.0f;
+    bool finished = false;
+    public TextMesh contador;
+    int characters = 0;
+    int[] frames;
+    float[] mirada;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frames = new int[] { 0,0,0 };
+        mirada = new float[] { 0.0f, 0.0f, 0.0f};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            finished = true;
+            contador.text = " " + mirada[0] * 100 / frames[0] + "%" + " " + mirada[1]*100/frames[1]+ "%" + " " + mirada[2] * 100 / frames[2] + "%";
+        }
+        else
+        {
+            contador.text = ""+timer;
+        }
+     //   Debug.Log(timer);
+        
+    }
+
+ 
+
+    public void frameLooking(int player)
+    {
+        if (!finished)
+        {
+            frames[player]++;
+            mirada[player] += 1.0f;
+        }
+    }
+    public void frameNotLooking(int player)
+    {
+        if (!finished)
+        {
+            frames[player]++;
+        }
+    }
+}
