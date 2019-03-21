21c37e77972e06f325bf5b58f1b5508d9f60c353
algaba97 <jalgaba@ucm.es>
2019-03-21 21:04:19 +0100

    json savedata

---
+++ Assets/Scripts/SaveDataManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class SaveDataManager : MonoBehaviour
+{
+    [Serializable]
+    class Data
+    {
+        public string info;
+        public float[] data;
+        public int epoch;
+
+    }
+    public int user = 0;
+    
+    public void SaveData(string info, float[] datos)
+    {
+        Data aux = new Data();
+        aux.info = info;
+        aux.data = new float[datos.Length];
+        aux.epoch = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+
+
+        //No veo otro metodo mejor para copiarlo
+        for (int i = 0; i< datos.Length; i++){
+            aux.data[i] = datos[i];
+        }
+
+        string json = JsonUtility.ToJson(aux);
+  
+        File.WriteAllText(Application.dataPath + "/" + "User" + user +"Type"+info+".json", json);
+
+    }
+
+        void Awake()
+    {
+        Debug.Log("just testing");
+        float[] datos = new float[] { 2.0f, 3.4f,0 };
+        SaveData("testinnng", datos);
+    }
+
+}
+
